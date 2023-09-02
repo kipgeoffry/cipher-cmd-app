85b970de5016d34f3850d0f28d24396faf7a0cf6
kipgeoffry <kipgeoffry@gmail.com>
2023-09-02 23:21:01 +0300

    added app to app entry point

--- src/main/java/net/kigen/CaesarCipher.java
+++ src/main/java/net/kigen/CaesarCipher.java
@@ -4,5 +4,7 @@ package net.kigen;
 public class CaesarCipher {
     public static void main(String[] args) {
         System.out.println("Welcome to Caesar Cipher Encryption and Decryption!");
+        FrontEnd ui = new FrontEnd();
+        ui.Operation();
     }
 }
