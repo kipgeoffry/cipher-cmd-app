b465781193b68e5caafac29a95333d0380d44d43
kipgeoffry <kipgeoffry@gmail.com>
2023-09-02 22:10:09 +0300

    added encrypt and decrypt functiong and setters

--- src/main/java/net/kigen/Cipher.java
+++ src/main/java/net/kigen/Cipher.java
@@ -1,4 +1,35 @@
 package net.kigen;
 
 public class Cipher {
+
+    private String message;
+    private int key;
+
+    public String encrypt(String message, int key){
+
+        return "";
+    };
+
+    public String decrypt(String message, int key){
+
+        return "";
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+
 }
