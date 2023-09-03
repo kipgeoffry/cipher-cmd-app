1b8322e29d776ea127c996680741effc5227285a
kipgeoffry <kipgeoffry@gmail.com>
2023-09-03 11:44:16 +0300

    added handling when key entered is out of range

--- src/main/java/net/kigen/FrontEnd.java
+++ src/main/java/net/kigen/FrontEnd.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class FrontEnd {
     Cipher cipher = new Cipher();
-    Scanner myScanner = new Scanner(System.in);
 
     //class constructor
     public FrontEnd(){
@@ -13,18 +12,18 @@ public class FrontEnd {
 
 
         while (true){
-
             System.out.print("Select an Operation to Perform: e to Encrypt or d to Decrypt:");
-            String ops = myScanner.nextLine().toLowerCase();
-            if( ops.startsWith("e")) {
-                String operation = "Encrypt";
-                userIO(operation,cipher.encrypt());
-            } else if (ops.startsWith("d")) {
-                String operation = "Decrypt";
-                userIO(operation,cipher.decrypt());
-            } else {
-                System.out.printf("%nInvalid Operation %n");
-            }
+                Scanner myScanner = new Scanner(System.in);
+                String ops = myScanner.nextLine().toLowerCase();
+                if( ops.startsWith("e")) {
+                    String operation = "Encrypt";
+                    userIO(operation,cipher.encrypt());
+                } else if (ops.startsWith("d")) {
+                    String operation = "Decrypt";
+                    userIO(operation,cipher.decrypt());
+                } else {
+                    System.out.printf("%nInvalid Operation %n");
+                }
             System.out.print("Do you want to Continue? Y or N :");
             String option = myScanner.next().toUpperCase();
             if(option.startsWith("N")){
@@ -37,12 +36,14 @@ public class FrontEnd {
     }
     private void userIO(String operation, String output){
         System.out.printf( "Enter the Message to %s:%n",operation);
+        Scanner myScanner = new Scanner(System.in);
         String message = myScanner.nextLine();
         cipher.setMessage(message);
         System.out.printf("Enter %sion key:", operation);
         int key = myScanner.nextInt();
-        if (key < 1 || key > 25){
+        if (key < 1 || key > 25 ){   //TODO handle when input is integer and out of range.
             System.out.println("Invalid key,Key should be a number from 1 to 25");
+            return;
         } else{
             cipher.setKey(key);
         }
